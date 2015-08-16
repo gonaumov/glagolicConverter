09d5b877b2a13f477bfba16bb9288fe476014e4a
Georgi Naumov <gonaumov@gmail.com>
2015-08-16 10:29:00 +0300

    Add all files from the old project.

---
+++ popup.js
@@ -0,0 +1,18 @@
+﻿$(document).on("click", "input#convertInput", function(event){
+	function handleConvert() {
+		this.empty();
+		$("<h1/>", {
+			text: $.convertCyrillicToGlagolitic($("#inputString").val())
+		}).appendTo(this);
+		this.show("slow");
+	}
+	
+	var outString = $("#outputString");
+	if ( outString.is(":visible") ) {
+		outString.hide("slow", function() {
+		  handleConvert.apply(outString);
+		});
+	} else {
+		handleConvert.apply(outString);
+	}
+});
